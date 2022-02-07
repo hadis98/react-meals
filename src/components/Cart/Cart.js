9bc597d6e9bd57db9e859fe4966ce93f732d24d6
hadis ghafouri <hadisghafouri98@gmail.com>
2022-02-07 15:23:17 +0330

    use checkout component in Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,18 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartContext = useContext(CartContext);
   const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
   const hasItems = cartContext.items.length > 0;
-  const cartItemRemoveHandler = (id) => {    
+  const cartItemRemoveHandler = (id) => {
     cartContext.removeItem(id);
   };
-  const cartItemAddHandler = (item) => {    
+  const cartItemAddHandler = (item) => {
     cartContext.addItem({ ...item, amount: 1 });
   };
   const cartItems = (
@@ -27,6 +29,22 @@ const Cart = (props) => {
       ))}
     </ul>
   );
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -34,12 +52,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount} </span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {!isCheckout && modalActions}
     </Modal>
   );
 };
